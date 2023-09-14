ddd970442cb65a73ebc8829568a7480c13971cb7
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-14 20:04:52 +0600

    fetched data form json file

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import Course from "./Course";
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("courses.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+
   return (
     <div className="w-3/4">
       <Course></Course>
