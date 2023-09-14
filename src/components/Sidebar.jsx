1ba96d8ec1e83c5d49f9d97d69a5a0670a3be572
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-14 21:13:48 +0600

    sidebar layout created

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,25 @@
 const Sidebar = () => {
   return (
-    <div className="w-1/4">
-      <h3>Sidebar</h3>
+    <div className="w-1/4 bg-white h-full p-6 rounded-md space-y-4 sticky top-2">
+      <h3 className="text-[#2F80ED] text-[1.125rem] font-bold">
+        Credit Hour Remaining 7 hr
+      </h3>
+
+      <hr />
+
+      <h3 className=" text-[1.25rem] font-bold">Course Name</h3>
+
+      <div>
+        <ol className="list-decimal ml-4 text-[#1c1b1b99]">
+          <li>Introduction to c programming</li>
+        </ol>
+      </div>
+
+      <hr />
+
+      <h4 className="font-medium">Total Credit Hour : 13 </h4>
+      <hr />
+      <h4 className="font-semibold">Total Price : 48000 USD </h4>
     </div>
   );
 };
